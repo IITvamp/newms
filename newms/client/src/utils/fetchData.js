dc800c07481482a76f1ebcea1cafb75528e8d581
iitvamp <ayush_b@ece.iitr.ac.in>
2023-03-29 12:03:52 +0530

    adding proxy with api

--- newms/client/src/utils/fetchData.js
+++ newms/client/src/utils/fetchData.js
@@ -3,8 +3,8 @@ import { SERVER_URL } from './config';
 
 // axios.defaults.withCredentials = true;
 export const getDataAPI = async (url, token) => {
-  console.log(`/api/${url}`)
-    const res = await axios.get(`/api/${url}`,{
+  console.log(`/${url}`)
+    const res = await axios.get(`/${url}`,{
         headers: { Authorization: token}
     });
   console.log(res)
@@ -25,14 +25,14 @@ export const postDataAPI = async (url, post, token) => {
 };
 
 export const putDataAPI = async (url, post, token) => {
-  const res = await axios.put(`/api/${url}`, post, {
+  const res = await axios.put(`/${url}`, post, {
     headers: { Authorization: token },
   });
   return res;
 };
 
 export const patchDataAPI = async (url, post, token) => {
-  const res = await axios.patch(`/api/${url}`, post, {
+  const res = await axios.patch(`/${url}`, post, {
     headers: { Authorization: token },
   });
   return res;
@@ -40,7 +40,7 @@ export const patchDataAPI = async (url, post, token) => {
 
 
 export const deleteDataAPI = async (url, token) => {
-  const res = await axios.delete(`/api/${url}`, {
+  const res = await axios.delete(`/${url}`, {
     headers: { Authorization: token },
   });
   return res;
